412fe40044d57cdcaffcc8afb2b2c6f26ba6e1b9
Haile-sys <gebrestadik26haile@gmail.com>
2026-09-15 04:47:18 -0700

    Update time function for Amharic time display
    Refactor time function to handle different time ranges and display time in Amharic.

--- timeindex.js
+++ timeindex.js
@@ -1,13 +1,29 @@
 function time(){
     const date = new Date();
     let hours = date.getHours();
-    const meridiem = hours > 12 ? 'PM' : 'AM';
-    hours = (hours - 12) + 6;
+    let meridiem;
+    if(hours < 7){
+        hours += 6; 
+        meridiem = "ከሌሊቱ ";
+    }
+    else if(hours > 6 && hours < 19){
+        hours -= 6;
+        if(hours < 6){
+            meridiem = "ከጠዋቱ "
+        }
+        else{
+            meridiem = "ከቀኑ ";
+        }
+    }
+    else{
+        hours -= 18;
+        meridiem = "ከሌሊቱ ";
+    }
     hours = hours.toString().padStart(2, "0");
-    const minut = date.getMinutes().toString().padStart(2, "0");
-    const sicond = date.getSeconds().toString().padStart(2, "0");
+    let minut = date.getMinutes().toString().padStart(2, "0");
+    let sicond = date.getSeconds().toString().padStart(2, "0");
 
-    document.getElementById("clock").textContent = `${hours}:${minut}:${sicond} ${meridiem}`
+    document.getElementById("clock").textContent = ` ${meridiem} ${hours}:${minut}:${sicond}`;
 }
 
 time();
